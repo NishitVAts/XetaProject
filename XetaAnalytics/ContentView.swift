9b619447c15e6b96a42c908a650b1d373371a386
NIshitVAts <nishitvats@icloud.com>
2024-04-06 08:02:12 +0530

    Initial Commit

---
+++ XetaAnalytics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XetaAnalytics
+//
+//  Created by Nishit Vats on 06/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XetaAnalytics/XetaAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XetaAnalyticsApp.swift
+//  XetaAnalytics
+//
+//  Created by Nishit Vats on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct XetaAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
